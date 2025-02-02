de837113ca21625affc24e3bd3461a2d1d7634e4
Sho <49664754+sho0722@users.noreply.github.com>
2025-02-02 16:35:28 +0900

    modified errors

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ const CACHE_NAME = 'CoffeeCherryChecker-v' + serviceWorkerVersion
 const CACHE_ASSETS = [
   '/',
   '/index.html',
+  '/manifest.json',
   '/assets/css/style.css',
   '/assets/css/ress.css',
   '/assets/css/splide.min.css',
@@ -15,33 +16,41 @@ const CACHE_ASSETS = [
   '/assets/images/img_bg.png',
   '/assets/icons/app_icon_512x512.png',
   '/assets/icons/app_icon_192x192.png',
-  '/assets/icons/app_icon_144x144.png',
   '/assets/icons/app_icon_96x96.png',
-  '/assets/icons/app_icon_72x72.png',
-  '/assets/icons/app_icon_48x48.png',
+  '/assets/icons/apple-touch-icon.png',
+  '/assets/icons/favicon.ico',
+  '/assets/icons/favicon.svg',
 ]
 
 // Install service worker
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing... version: ' + serviceWorkerVersion + ' cacheName:' + CACHE_NAME) 
-  self.skipWaiting()
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('[Service Worker] Caching all: app shell and content')
-      return cache.addAll(CACHE_ASSETS)
+    caches.open(CACHE_NAME).then(async function (cache) {
+      console.log('[Service Worker] Caching all: app shell and content');
+
+      const cachePromises = CACHE_ASSETS.map(url => {
+        return fetch(url).then(response => {
+          if (!response.ok) throw new Error(`Failed to fetch ${url}`);
+          return cache.put(url, response);
+        }).catch(error => {
+          console.warn(`Skipping ${url}:`, error);
+        });
+      });
+
+      return Promise.all(cachePromises);
+    }).then(() => {
+      self.skipWaiting();
     })
-  )
+  );
 })
 
 // Catch files
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (r) {
-      // console.log('[Service Worker] Fetching resource: ' + event.request.url) // For Debug
-      // we don't store falsy objects, so '||' works fine here.
       return r || fetch(event.request).then(function (response) {
         return caches.open(CACHE_NAME).then(function (cache) {
-          // console.log('[Service Worker] Caching new resource: ' + event.request.url) // For Debug
           cache.put(event.request, response.clone())
           return response
         })
@@ -57,7 +66,6 @@ self.addEventListener('activate', (event) => {
 
   event.waitUntil(
     caches.keys().then((keyList) => {
-      // eslint-disable-next-line array-callback-return
       return Promise.all(keyList.map((key) => {
         if (cacheAllowlist.indexOf(key) === -1) {
           console.log('[Service Worker] deleting old cache: ' + CACHE_NAME)
