cd12820deaa27f6cac3acaa78cacd0fd4e19af6c
Sho <49664754+sho0722@users.noreply.github.com>
2025-03-20 01:02:53 +0200

    New Ver.0.1.8

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.1.7'; // Change version number when updating
+const CACHE_VERSION = '0.1.8'; // Change version number when updating
 const CACHE_NAME = 'CoffeeCherryChecker-v' + CACHE_VERSION;
 const CACHE_ASSETS = [
   '/',
